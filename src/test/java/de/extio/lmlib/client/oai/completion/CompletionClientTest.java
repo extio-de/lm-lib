63356ab5a771162db04af0974cd89882446da540
extio-de <github@extio.de>
2026-05-14 20:29:29 +0200

    fix: add @Qualifier annotation to ChatCompletionClient for proper dependency injection

--- src/test/java/de/extio/lmlib/client/oai/completion/CompletionClientTest.java
+++ src/test/java/de/extio/lmlib/client/oai/completion/CompletionClientTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -50,6 +51,7 @@ public class CompletionClientTest {
 	private TextCompletionClient textCompletionClient;
 	
 	@Autowired
+	@Qualifier("chatCompletionClient")
 	private ChatCompletionClient chatCompletionclient;
 	
 	@Autowired
